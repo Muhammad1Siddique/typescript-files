c0daae101287cbbb8b3221113b070fa73a355472
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-05-23 12:47:36 +0500

    First Typescript update

---
+++ src/sample.js
@@ -0,0 +1,10 @@
+console.log("Pakistan Zindabad!");
+var a = "Pakistan";
+//a = true; bolean type not allow
+console.log(a);
+var student = {
+    'name': "Hassan",
+    'class': "18"
+};
+//console.log(student.age); age property is not exist
+console.log(student.name);
---
+++ src/sample.ts
@@ -0,0 +1,12 @@
+console.log("Pakistan Zindabad!");
+
+let a = "Pakistan";
+//a = true; bolean type not allow
+console.log(a);
+
+let student = {
+    'name' : "Hassan",
+    'class': "18"
+};
+//console.log(student.age); age property is not exist
+console.log(student.name);
