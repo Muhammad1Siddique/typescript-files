ade60108fff3ff588acab1a06bdcc94bd067f1ff
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-05-23 14:53:12 +0500

    Tutorial 3 Example

---
+++ dist/tutorial3.js
@@ -0,0 +1,15 @@
+"use strict";
+var myName = "Siddique";
+//myName = 24; other datatype not allow
+myName = "Muhammad Siddique";
+var age = 23;
+//age = "sial"; other datatype are not allow
+age = 29;
+var isBlackBelt = false;
+//isBlackBelt = 'game' other datatype are not allow 
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+//console.log(circ("game")); other datatype are not allow
+console.log(circ(32));
---
+++ src/tutorial3.ts
@@ -0,0 +1,18 @@
+let myName = "Siddique";
+//myName = 24; other datatype not allow
+myName = "Muhammad Siddique";
+
+let age = 23;
+//age = "sial"; other datatype are not allow
+age = 29;
+
+let isBlackBelt = false;
+//isBlackBelt = 'game' other datatype are not allow 
+isBlackBelt = true;
+
+const circ = (diameter: number) =>{
+    return diameter * Math.PI;
+}
+
+//console.log(circ("game")); other datatype are not allow
+console.log(circ(32));
